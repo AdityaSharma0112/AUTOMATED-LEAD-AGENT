7b5625a0f189ce385c4064ddec0ec35aff42e107
Aditya <aditya0112sharma@gmail.com>
2026-09-16 22:35:39 +0530

    helth check uptime

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from leads.views import health_check
 
 urlpatterns = [
+    path('', health_check, name='root_home'),
     path('health', health_check, name='root_health_check'),
     path('admin/', admin.site.urls),
     path('api/', include('leads.urls')),
